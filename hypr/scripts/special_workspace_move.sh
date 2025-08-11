70c2ebd827d3dfde09be50a02caef717a56cf74a
Grzrzegorzrz <wnguye4@uwo.ca>
2025-08-11 13:58:06 -0400

    Fix: moving to special workspace i now works properly

--- hypr/scripts/special_workspace_move.sh
+++ hypr/scripts/special_workspace_move.sh
@@ -5,34 +5,34 @@ output="$(hyprctl dispatch movetoworkspace "r+0")"
 if [ "$output" = "Not moving to workspace because it didn't change." ]; then
 
   if (( $# == 0 )); then
-    $(hyprctl dispatch movetoworkspace "special")
+    hyprctl dispatch movetoworkspace "special:special"
   fi
 
   while getopts 'dtsveu' flag; do
     case $flag in
       d)
-        $(hyprctl dispatch movetoworkspace "special:discord")
+        hyprctl dispatch movetoworkspace "special:discord"
       ;;
       u)
-        $(hyprctl dispatch movetoworkspace "special:thunderbird")
+        hyprctl dispatch movetoworkspace "special:thunderbird"
       ;;
       s)
-        $(hyprctl dispatch movetoworkspace "special:spotify")
+        hyprctl dispatch movetoworkspace "special:spotify"
       ;;
       b)
-        $(hyprctl dispatch movetoworkspace "special:slack")
+        hyprctl dispatch movetoworkspace "special:slack"
       ;;
       v)
-        $(hyprctl dispatch movetoworkspace "special:vertical")
+        hyprctl dispatch movetoworkspace "special:vertical"
       ;;
       e)
-        $(hyprctl dispatch movetoworkspace "special:e")
+        hyprctl dispatch movetoworkspace "special:e"
       ;;
       t)
-        $(hyprctl dispatch movetoworkspace "special:top")
+        hyprctl dispatch movetoworkspace "special:top"
       ;;
       *)
-        $(hyprctl dispatch movetoworkspace "special")
+        hyprctl dispatch movetoworkspace "special:special"
       ;;
     esac
   done
