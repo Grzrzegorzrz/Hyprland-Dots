2357b3fe64351e2de66e2fba30c4eb17a5007359
Grzrzegorzrz <wnguye4@uwo.ca>
2025-11-09 00:10:05 -0500

    background fade

---
+++ hypr/scripts/background_fade.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ "$(hyprctl getoption decoration:dim_special | awk 'NR==1{print $2}')" == "0.200000" ]]; then
+  hyprctl keyword decoration:dim_special 0
+else
+  hyprctl keyword decoration:dim_special 0.2
+fi
